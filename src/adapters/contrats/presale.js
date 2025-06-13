be3755051e0a5d85c6be5c3198c6cf3b21c523b0
mephistopheles1010 <47539531+mephistopheles1010@users.noreply.github.com>
2025-06-13 10:54:38 +0300

    add contract mocked adapter

---
+++ src/adapters/contrats/presale.js
@@ -0,0 +1,10 @@
+export class PresaleContract {
+  constructor() {
+
+  }
+
+  async addUser(wallet) {
+    // FIXME: implement
+    console.log(`Attempt to add wallet(${wallet}) to presale contract user pool`);
+  }
+}

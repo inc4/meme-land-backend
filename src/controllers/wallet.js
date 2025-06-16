d464b9c0804d1d3aab62297a1c74b16160288d80
mephistopheles1010 <47539531+mephistopheles1010@users.noreply.github.com>
2025-06-16 15:22:39 +0300

    add openapi annotation for wallets controllers

--- src/controllers/wallet.js
+++ src/controllers/wallet.js
@@ -1,3 +1,44 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     WalletInput:
+ *       type: object
+ *       required:
+ *         - wallet
+ *         - referrerInviteCode
+ *       properties:
+ *         wallet:
+ *           type: string
+ *           example: A86EzVnP4VnHUKoK1w2DkZ1pYN17U7751YTMNEyGmEmp
+ *         referrerInviteCode:
+ *           type: string
+ *           example: d3c6861a-062d-489a-9837-1fb84d0a7361
+ *     WalletOutput:
+ *       type: object
+ *       properties:
+ *         referrer:
+ *           type: string
+ *           example: A86EzVnP4VnHUKoK1w2DkZ1pYN17U7751YTMNEyGmEmp
+ *         wallet:
+ *           type: string
+ *           example: A86EzVnP4VnHUKoK1w2DkZ1pYN17U7751YTMNEyGmEmp
+ *         inviteCode:
+ *           type: string
+ *           example: d3c6861a-062d-489a-9837-1fb84d0a7361
+ *         isAdmin:
+ *           type: boolean
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           example: "2025-06-12T12:34:56.789Z"
+ *     InviteCode:
+ *       type: object
+ *       properties:
+ *         inviteCode:
+ *           type: string
+ *           example: d3c6861a-062d-489a-9837-1fb84d0a7361
+ */
 export class WalletController {
   #service;
   #composeError;
@@ -15,7 +56,33 @@ export class WalletController {
     router.put('/wallets/:walletId/invite-code', this.updateInviteCode.bind(this));
   }
 
-  // POST: /wallet {referrerInviteCode: , wallet:}
+  /**
+   * @openapi
+   * /wallets:
+   *   post:
+   *     summary: Add provided wallet address to user pool, previously verify referrerInviteCode
+   *     requestBody: 
+   *      required: true
+   *      description: Properties required to add wallet
+   *      content:
+   *       application/json:
+   *        schema:
+   *          $ref: "#/components/schemas/WalletInput"
+   *     responses:
+   *       201:
+   *         description: Added wallet info
+   *         content:
+   *          application/json:
+   *            schema:
+   *              $ref: "#/components/schemas/WalletOutput"
+   *          
+   *       400:
+   *         $ref: "#/components/responses/InvalidInputs"
+   *       401:
+   *         $ref: "#/components/responses/Unauthorized"
+   *       500:
+   *         $ref: "#/components/responses/InternalServerError"
+   */
   async addSingle(req, res, next) {
     try {
       if (req.auth.requester != req.body.wallet) {
@@ -34,7 +101,27 @@ export class WalletController {
     }
   }
 
-  //  GET: /wallet/{ wallet_address }, wallet.inviteCode
+
+  /**
+   * @openapi
+   * /wallets/{walletId}:
+   *   get:
+   *     summary: get wallet by wallet address
+   *     responses:
+   *       200:
+   *         description: Specified by walletId wallet info
+   *         content:
+   *          application/json:
+   *            schema:
+   *              $ref: "#/components/schemas/WalletOutput"
+   *          
+   *       404:
+   *         $ref: "#/components/responses/NotFound"
+   *       401:
+   *         $ref: "#/components/responses/Unauthorized"
+   *       500:
+   *         $ref: "#/components/responses/InternalServerError"
+   */
   async getSingle(req, res, next) {
     try {
       if (req.auth.requester != req.params.walletId) {
@@ -53,6 +140,32 @@ export class WalletController {
 
 
   // PUT: /wallet/{ wallet_address }/invite-code, , with empty body{}
+  /**
+   * @openapi
+   * /wallets:
+   *   put:
+   *     summary: Regenerate wallet invite code 
+   *     requestBody: 
+   *      description: Empty body
+   *      content:
+   *       application/json:
+   *        schema:
+   *          $ref: "#/components/schemas/EmptyBody"
+   *     responses:
+   *       200:
+   *         description: Newly generated invite code
+   *         content:
+   *          application/json:
+   *            schema:
+   *              $ref: "#/components/schemas/InviteCode"
+   *          
+   *       400:
+   *         $ref: "#/components/responses/InvalidInputs"
+   *       401:
+   *         $ref: "#/components/responses/Unauthorized"
+   *       500:
+   *         $ref: "#/components/responses/InternalServerError"
+   */
   async updateInviteCode(req, res, next) {
     try {
       if (req.auth.requester != req.params.walletId) {
@@ -60,7 +173,7 @@ export class WalletController {
       }
       const inviteCode = await this.#service.updateInviteCode(req.params.walletId);
       if (!inviteCode) {
-        return res.status(404).send(this.#composeError(400, 'Invalid wallet address'));
+        return res.status(400).send(this.#composeError(400, 'Invalid wallet address'));
       }
       return res.status(200).send(inviteCode);
     } catch (err) {

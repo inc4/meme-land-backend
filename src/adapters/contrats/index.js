b22195e4fcc700f2477d718e2fb28ab561975bb2
chukFilovskii <chuk_filovskii@proton.me>
2025-06-17 18:39:24 +0300

    add tokenContractAdapter mock

--- src/adapters/contrats/index.js
+++ src/adapters/contrats/index.js
@@ -1,1 +1,2 @@
-export { PresaleContract } from './presale.js';
+export { PresaleContract }  from './presale.js';
+export { TokenContract } from './token.js'
---
+++ src/adapters/contrats/token.js
@@ -0,0 +1,11 @@
+export class TokenContract {
+  constructor() {
+
+  }
+
+  async createToken(name, symbol, uri) {
+    // FIXME: implement
+    console.log(`Attempt to create token(${name}) to token contract user pool`);
+  }
+}
+

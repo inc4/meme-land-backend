66d2c9a65f6873fddd60a18f994c54235f3a61b9
mephistopheles1010 <47539531+mephistopheles1010@users.noreply.github.com>
2025-06-18 19:58:24 +0300

    add retrieve wallets list api

--- src/libs/data-page-composer/data-page-composer.js
+++ src/libs/data-page-composer/data-page-composer.js
@@ -1,15 +1,22 @@
 export class DataPageComposer {
-  static composePageInfo(data, page, collectionTotalLength) {
+  // Create data page info from mongoose-paginate-v2 paginate call result.
+  // result = {
+  //   "docs": [...],
+  //   "totalDocs": 100,
+  //   "limit": 10,
+  //   "page": 1,
+  //   "totalPages": 10,
+  //   "pagingCounter": 1,
+  //   "hasPrevPage": false,
+  //   "hasNextPage": true
+  // }
+  static composePageInfo(result) {
     return {
-      totalItems: collectionTotalLength,
+      totalItems: result.totalDocs,
       page: {
-        index: page?.index ?? 0,
-        size: data.length,
-        sort: {
-          property: page?.sort?.property,
-          direction: page?.sort?.direction,
-        },
-        data: data,
+        index: result.page - 1, // paginate use 1 as first page
+        size: result.docs.length,
+        data: result.docs,
       },
     };
   }

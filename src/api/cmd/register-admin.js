12c46f0d62a7a6a09ece0fb6ef985961447196bd
mephistopheles1010 <47539531+mephistopheles1010@users.noreply.github.com>
2025-07-08 02:33:50 +0300

    pre-register only not existed admins

--- src/api/cmd/register-admin.js
+++ src/api/cmd/register-admin.js
@@ -1,35 +1,48 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
+import winston from 'winston';
 
-import { PresaleContractAdapter, } from '../../adapters/index.js';
+import { PresaleContractAdapter, WinstonLoggerAdapter } from '../../adapters/index.js';
 import { walletModel, WalletService } from '../../domain/index.js';
+import { DataPageComposer } from '../../libs/index.js'
 import config from '../../../config/index.js';
 
 
 
 async function main() {
-  const adminWalletList = process.env.ADMIN_WALLET;
+  try {
+    const adminWalletList = process.env.ADMIN_WALLET;
 
-  if (!adminWalletList) {
-    throw Error('No admin wallets specified');
+    if (!adminWalletList) {
+      throw Error('No admin wallets specified');
+    }
+
+    await mongoose.connect(config.mongo.url, config.mongo.options);
+    const logger = new WinstonLoggerAdapter(winston, config.logger);
+    const presaleContractAdapter = new PresaleContractAdapter(logger, config.anchorOptions, false);
+    const walletService = new WalletService(
+      walletModel,
+      presaleContractAdapter,
+      DataPageComposer.composePageInfo,
+      logger
+    );
+
+    const wallets = adminWalletList.split(' ');
+
+    for (const wallet of wallets) {
+      let walletInfo = await walletService.getSingle(wallet);
+      if (!walletInfo) {
+        walletInfo = await walletService.addSingle({ referrer: wallet, wallet: wallet, isAdmin: true });
+        logger.info('Admin wallet successfully registered: ', walletInfo);
+      } else {
+        logger.info('Admin wallet previously existed: ', walletInfo);
+      }
+    }
+  } catch (err) {
+    logger.error('Admin wallet registration error: ', err);
   }
-  
-  await mongoose.connect(config.mongo.url, config.mongo.options);
-  const presaleContractAdapter = new PresaleContractAdapter();
-  const walletService = new WalletService(walletModel, presaleContractAdapter);
-
-  const createdWalletPromises = adminWalletList.split(' ').map((wallet) => {
-    return walletService.addSingle({ referrer: wallet, wallet: wallet, isAdmin: true });
-  });
-
-  return await Promise.all(createdWalletPromises);
+  process.exit(0);
 }
 
-main()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+main();
